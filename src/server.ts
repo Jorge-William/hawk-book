45512e79b04ad4db780fb448bc16d51f9abf8fb9
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-04-06 22:32:21 -0300

    Fix: Trocando o Festify pelo Express

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
-import Fastify from 'fastify'
-import cors from  '@fastify/cors'
-import { routesBook } from './routes'
-const app = Fastify()
+import cors from  'cors'
+import rotas from './routes/index'
+import bodyParser from 'body-parser'
 
-app.register(cors)
-app.register(routesBook)
+import express from 'express'
+
+const app = express()
+
+const corsOptions = {
+    origin: "http://localhost:5001/",
+  };
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(rotas)
+app.use(express.static("build"))
+app.use(cors(corsOptions));
+const PORT = process.env.PORT
 
 
-app.listen({port: 3000}, () => {
-    console.log(app.server.address());
+app.listen(PORT, () => {
+    console.log(`SERVIDOR RODANDO NA PORTA: ${PORT}`);
 })
  
